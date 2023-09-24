857abbc51dee07c4964b8428df077894f1be1219
shrohit-007 <shakyarohit552@gmail.com>
2023-09-25 05:02:34 +0530

    Updates

---
+++ precache-manifest.fd0b6eecc8ac7250d0924634f3440849.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f32014c849e9e2c152cc888a075e263",
+    "url": "/Note-Keep/index.html"
+  },
+  {
+    "revision": "2ec50e8d04ad6520f3e0",
+    "url": "/Note-Keep/static/js/2.86d630e7.chunk.js"
+  },
+  {
+    "revision": "b45caac4aaa9101edb65",
+    "url": "/Note-Keep/static/js/main.0b4d122a.chunk.js"
+  },
+  {
+    "revision": "c48e11ee207a435869aa",
+    "url": "/Note-Keep/static/js/runtime-main.17696780.js"
+  }
+]);
